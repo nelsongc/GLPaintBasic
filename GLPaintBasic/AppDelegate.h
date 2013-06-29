a8ed909124105c2853d1aa1cea161219be43d5be
Nelson Garcia <nelsongarcia.info@gmail.com>
2013-06-29 00:01:00 -0400

    Initial Commit

---
+++ GLPaintBasic/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GLPaintBasic
+//
+//  Created by Nelson G on 6/29/13.
+//  Copyright (c) 2013 ObjectGR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
